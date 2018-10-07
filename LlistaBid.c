5c1951109c636ab7fcd7364b2df389f5da2606fe
alexanderroca <alexander.roca@students.salle.url.edu>
2018-10-07 17:15:54 +0200

    //feat: the following features have been added: LLISTABID_vesInici, LLISTABID_vesFinal, LLISTABID_avanca, LLISTABID_retrocedeix, LLISTABID_inserir, LLISTABID_insereixOrdenat, LLISTABID_consulta, LLISTABID_esborra, LLISTABID_destrueix

--- LlistaBid.c
+++ LlistaBid.c
@@ -50,9 +50,109 @@ int LLISTABID_esBuida (LlistaBid l){
 }
 
 /*
+Procedure that puts the PDI to the first node
+*/
+void LLISTABID_vesInici (LlistaBid* l){
+	l -> pdi = l -> pri -> seg;
+}
+
+/*
+Procedure that puts the PDI to the last node
+*/
+void LLISTABID_vesFinal (LlistaBid* l){
+	l -> pdi = l -> ult -> ant;
+}
+
+/*
+Procedure that shift the PDI to the right node
+*/
+void LLISTABID_avanca (LlistaBid* l){
+	if(l -> pdi != l -> ult){
+		l -> pdi = l -> pdi -> seg;
+	}
+}
+
+/*
+Procedure that shift the PDI to the left node
+*/
+void LLISTABID_retrocedeix(LlistaBid *l){
+	if (l -> pdi != l-> pri) {
+		l -> pdi = l -> pdi -> ant;
+	}
+}
+
+/*
+
+*/
+void LLISTABID_insereixOrdenat(LlistaBid *l, int num){
+  int trobat = 0;
+
+  LLISTABID_vesInici(l);
+  while(!trobat){
+    if(l->pdi){
+
+        LLISTABID_avanca (l);
+    } //if
+    else{
+      trobat = 1;
+    } //else
+  } //while
+
+  LLISTABID_inserir (l, num);
+}
+
+/*
+Procedure that adds an element to the right of the PDI
+*/
+void LLISTABID_inserir (LlistaBid* l, int num){
+	Node* aux = NULL;
+	if(l -> pdi -> seg != NULL){
+		aux= (Node*) malloc (sizeof (Node));
+		if(aux != NULL){
+			aux -> num = num;
+			aux -> ant = l -> pdi;
+			aux -> seg = l -> pdi -> seg;
+			l -> pdi -> seg -> ant = aux;
+			l -> pdi -> seg = aux;
+			l -> pdi = aux;
+		}
+	}
+}
+
+
+/*
+This function returns the number that is in the PDI
+*/
+int LLISTABID_consulta (LlistaBid l){
+	int num;
+	if(l.pdi == l.pri || l.pdi == l.ult){
+		num = ELEMENT_INDEFINIT;
+	}
+	else{
+		num = l.pdi -> num;
+	}
+	return num;
+}
+
+
+/*
+This procedure clears the PDI by making a free one of the node once it is left out of the list
+*/
+void LLISTABID_esborra (LlistaBid* l){
+	Node* n;
+	if(l -> pdi -> ant != NULL && l -> pdi -> seg != NULL){
+		l -> pdi -> ant -> seg = l -> pdi -> seg;
+		l -> pdi -> seg -> ant = l -> pdi -> ant;
+		n = l -> pdi;
+		l -> pdi = l -> pdi -> seg;
+		free (n);
+	}
+}
+
+/*
 Procedure that destroy the entire list leaving the PDI null
 */
-/*void LLISTABID_destrueix(LlistaBid *l){
+void LLISTABID_destrueix(LlistaBid *l){
 	LLISTABID_vesInici(l);
 	while (!LLISTABID_esBuida(*l)){
 		LLISTABID_esborra(l);
@@ -62,4 +162,3 @@ Procedure that destroy the entire list leaving the PDI null
 	l -> pdi = NULL;
 	l -> ult = NULL;
 }
-*/
--- LlistaBid.h
+++ LlistaBid.h
@@ -18,10 +18,18 @@ typedef struct{
   Node *pdi;
 }LlistaBid;
 
-LlistaBid LLISTABID_crea(); //TODO: test
-int LLISTABID_inici(LlistaBid l); //TODO: test
-int LLISTABID_fi (LlistaBid l); //TODO: test
-int LLISTABID_esBuida (LlistaBid l);  //TODO: test
-//void LLISTABID_destrueix(LlistaBid *l); //TODO: test
+LlistaBid LLISTABID_crea();
+int LLISTABID_inici(LlistaBid l);
+int LLISTABID_fi (LlistaBid l);
+int LLISTABID_esBuida (LlistaBid l);
+void LLISTABID_vesInici (LlistaBid* l);                 //TODO: test
+void LLISTABID_vesFinal (LlistaBid* l);                 //TODO: test
+void LLISTABID_avanca (LlistaBid* l);                   //TODO: test
+void LLISTABID_retrocedeix(LlistaBid *l);               //TODO: test
+void LLISTABID_inserir (LlistaBid* l, int num);         //TODO: test
+void LLISTABID_insereixOrdenat(LlistaBid *l, int num);  //TODO: test
+int LLISTABID_consulta (LlistaBid l);                   //TODO: test
+void LLISTABID_esborra (LlistaBid* l);                  //TODO: test
+void LLISTABID_destrueix(LlistaBid *l);                 //TODO: test
 
 #endif
