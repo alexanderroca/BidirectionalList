fa5e68b1df3111c85906cf26b81929651612e661
alexanderroca <alexander.roca@students.salle.url.edu>
2018-10-07 20:00:27 +0200

    //error de commit

--- main.c
+++ main.c
@@ -49,6 +49,58 @@ int main(){
     return 1;
   } //else
 
+  printf("Attempt to insert number 2\n");
+  LLISTABID_insereixOrdenat (&list, 2);
+  printf("Check the node inserted\n");
+  if(LLISTABID_consulta (list) == 2){
+    printf("The query was successful\n");
+  } //if
+  else{
+    return 1;
+  } //else
+
+  printf("Attempt to insert number 8\n");
+  LLISTABID_insereixOrdenat (&list, 8);
+  printf("Check the node inserted\n");
+  if(LLISTABID_consulta (list) == 8){
+    printf("The query was successful\n");
+  } //if
+  else{
+    return 1;
+  } //else
+
+  printf("We show all the inserted elements from the first node until the last\n");
+  LLISTABID_vesInici(&list);
+  while(list.pdi -> num != -1){
+    printf("%d\n", LLISTABID_consulta (list));
+    LLISTABID_avanca (&list);
+  } //while
+
+  printf("We show all the inserted elements from the last node until the first\n");
+  LLISTABID_vesFinal(&list);
+  while(list.pdi -> num != -1){
+    printf("%d\n", LLISTABID_consulta (list));
+    LLISTABID_retrocedeix(&list);
+  } //while
+
+  printf("We will delete an item from the list\n");
+  LLISTABID_avanca (&list);
+  LLISTABID_esborra (&list);
+  printf("We show all the inserted elements from the first node until the last to check the deleted item\n");
+  LLISTABID_vesInici(&list);
+  while(list.pdi -> num != -1){
+    printf("%d\n", LLISTABID_consulta (list));
+    LLISTABID_avanca (&list);
+  } //while
+
+  printf("We will delete the entire list\n");
+  LLISTABID_destrueix(&list);
+
+  if(!LLISTABID_esBuida (list)){
+    printf("Error, list is not destroyed\n");
+    return 1;
+  } //if
+
   printf("\nAll OK\n");
 	return 0;
 }
